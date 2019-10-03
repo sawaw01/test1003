e0c14ec2b86f962f90479f7948b2b349bba9d851
15138 <takaya151383515@docomo.ne.jp>
2019-10-03 10:17:27 +0900

    (Dがキャラ移動処理を追加)

--- test1003/D.java
+++ test1003/D.java
@@ -14,6 +14,9 @@ public class D extends Actor
      */
     public void act() 
     {
-        // Add your action code here.
+        if( Greenfoot.isKeyDown( "left" ) ){
+        setRotation(0);
+        move(-1);
+}// Add your action code here.
     }    
 }
